b48c3530b53f569787e78af599c2473eff87bc9b
Allison Sichling <saphron.scul@gmail.com>
2016-01-01 23:53:17 -0500

    sync router is successfully connecting to weather API and console.loging data

--- routes/sync.js
+++ routes/sync.js
@@ -8,14 +8,22 @@ if (!process.env.WEATHER_ID) {
 router.get('/',
   function (req, res) {
     var weatherID = process.env.WEATHER_ID;
-    http.get('http://api.openweathermap.org/data/2.5/weather?zip=02141,us&APPID=' + weatherID, function(res) {
-      console.log("Got response: " + res.statusCode);
-      console.log('pressure: ' + res.body.main.pressure);
-      console.log('zipcode: ' + res.body.zipcode);
-      res.resume();
+    http.get('http://api.openweathermap.org/data/2.5/weather?zip=02141,us&APPID=' + weatherID, function (response) {
+      var body = '';
+      response.on('data', function(d) {
+        body += d;
+      });
+      response.on('end', function() {
+        // Data reception is done, do whatever with it!
+        var parsed = JSON.parse(body);
+        console.log("Got response: " + res.statusCode);
+        console.log('pressure: ' + parsed.main.pressure);
+        console.log('zipcode: ' + parsed.zipcode);
+      });
     }).on('error', function(e) {
       console.log("Got error: " + e.message);
     });
+    res.sendStatus(200);
   });
 
 module.exports = router;
