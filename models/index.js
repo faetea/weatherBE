6a049aa3c7998a4806e9ca7392fdd526fd63da48
Allison Sichling <saphron.scul@gmail.com>
2016-01-13 22:51:58 -0500

    maybe typo

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize');
 // connection via universal reference identifier
 var sequelize = new Sequelize(
 //  'postgres://faetea@localhost:5432/pressure'
-  'process.env.DATABASE_URL'
+  process.env.DATABASE_URL
 );
 
 module.exports = sequelize;
