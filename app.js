04bcea65e5f6b3111564aca2a1a3037eb56e2ff0
Allison Sichling <saphron.scul@gmail.com>
2016-01-13 21:29:48 -0500

    session

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require('express-session')({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(require('body-parser').json({ extended: true }));
 
 app.use(require('cors')({
